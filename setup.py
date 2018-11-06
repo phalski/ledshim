971959df79a6cf7a96fd09c722d98583a976edf9
Philipp Michalski <mail@phalski.com>
2018-11-06 13:54:50 +0000

    version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='phalski-ledshim',
-    version='0.0.1',
+    version='0.1.0',
     packages=['phalski_ledshim', 'phalski_ledshim.animation', 'phalski_ledshim.charting', 'phalski_ledshim.threading'],
     url='https://github.com/phalski/phalski-ledshim',
     license='MIT',
