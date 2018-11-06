88fe1da0c2b268249e4feeafce0446934c6b014d
Philipp Michalski <mail@phalski.com>
2018-11-06 14:37:41 +0000

    prepared pypi release

--- setup.py
+++ setup.py
@@ -2,14 +2,15 @@ from setuptools import setup
 
 setup(
     name='phalski-ledshim',
-    version='0.1.0',
+    version='0.1.1',
     packages=['phalski_ledshim', 'phalski_ledshim.animation', 'phalski_ledshim.charting', 'phalski_ledshim.threading'],
     url='https://github.com/phalski/phalski-ledshim',
-    license='MIT',
+    license='LICENSE.txt',
     author='phalski',
     author_email='mail@phalski.com',
     description='A basic application framework for the Pimoroni LED SHIM',
+    long_description=open('README.rst').read(),
     install_requires=[
-        'ledshim==0.0.1'
+        'ledshim>=0.0.1'
     ]
 )
